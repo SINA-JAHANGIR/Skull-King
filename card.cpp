6ed97b2146da32454d48e66226e412be4438190f
SINA <sina.jahangir.013@gmail.com>
2023-06-13 19:19:09 +0330

    Write setter and getter to card class

--- card.cpp
+++ card.cpp
@@ -3,5 +3,36 @@
 card::card(QWidget *parent)
     : QWidget{parent}
 {
+    value = 0 ;
+    selected = false;
+}
 
+void card::set_type(const card_type& input)
+{
+    this->type = input;
+}
+
+const card_type card::get_type()
+{
+    return type;
+}
+
+void card::set_value(const int& input)
+{
+    this->value = input;
+}
+
+const int card::get_value()
+{
+    return value;
+}
+
+void card::set_selected(const bool& input)
+{
+    this->selected = input;
+}
+
+const bool card::get_selected()
+{
+    return selected;
 }
--- card.h
+++ card.h
@@ -15,6 +15,14 @@ private :
     Q_OBJECT
 public:
     explicit card(QWidget *parent = nullptr);
+    // Functions :
+    void set_type(const card_type&);
+    const card_type get_type();
+    void set_value(const int&);
+    const int get_value();
+    void set_selected(const bool&);
+    const bool get_selected();
+
 
 signals:
 
