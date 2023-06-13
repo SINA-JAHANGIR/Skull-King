660ffaf30772156933fdd50c1c622d4986a914d0
Mehdi Vakili <mehdivakili79@gmail.com>
2023-06-13 10:43:53 +0330

    Add person class

---
+++ person.h
@@ -0,0 +1,19 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <QString>
+
+class person
+{
+private :
+    QString name;
+    QString username;
+    QString password;
+    QString phone;
+    QString email;
+    int coin;
+public:
+    person();
+};
+
+#endif // PERSON_H
